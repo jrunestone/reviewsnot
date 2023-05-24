e697574959d9cd0e3dc68efc7414ff60e250ca34
Johan Runsten <johanrunsten86@gmail.com>
2023-05-24 15:54:52 +0200

    Correct paths

--- src/components/rating/index.jsx
+++ src/components/rating/index.jsx
@@ -4,11 +4,11 @@ export default function Rating(props) {
   return (
     <div className={styles["rating"]}>
       {Array.from(Array(props.on).keys()).map(i => (
-        <img src="/star.svg" alt="star" width="32" height="32" className={styles["rating-star"]} key={`on-${i}`} />
+        <img src="/static/star.svg" alt="star" width="32" height="32" className={styles["rating-star"]} key={`on-${i}`} />
       ))}
 
       {Array.from(Array(5 - props.on).keys()).map(i => (
-        <img src="/star.svg" alt="star" width="32" height="32" className={styles["rating-star-off"]} key={`off-${i}`} />
+        <img src="/static/star.svg" alt="star" width="32" height="32" className={styles["rating-star-off"]} key={`off-${i}`} />
       ))}
     </div>
   )
