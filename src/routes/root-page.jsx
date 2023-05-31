ecacc4aed6dad00694efc729b44142cdcd78613c
Johan Runsten <johanrunsten86@gmail.com>
2023-05-31 21:14:55 +0200

    Click for more

--- src/routes/root-page.jsx
+++ src/routes/root-page.jsx
@@ -1,8 +1,10 @@
 import { Outlet } from "react-router-dom";
 
+const goToRandomItem = () => window.location = '/random';
+
 export default function RootPage() {
   return (
-    <div id="root-page" className="component">
+    <div id="root-page" className="component" onClick={goToRandomItem}>
       <Outlet />
     </div>
   );
